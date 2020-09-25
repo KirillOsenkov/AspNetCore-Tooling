92f4f77b520a14693b7a1beb6d88cc760c640128
Kirill Osenkov <github@osenkov.com>
2020-09-24 23:13:53 -0700

    IDE changes.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -pack %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -pack -binaryLog /p:DotNetUseShippingVersions=true /p:DebugType=embedded %*"
 exit /b %ErrorLevel%
